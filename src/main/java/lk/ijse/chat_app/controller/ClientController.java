54f3b4f5cd4757a799a702661b73229f85b7acbb
sasmithx <sasmithgit@gmail.com>
2024-01-19 07:38:31 +0530

    ClientController updtd

--- src/main/java/lk/ijse/chat_app/controller/ClientController.java
+++ src/main/java/lk/ijse/chat_app/controller/ClientController.java
@@ -1,4 +1,71 @@
 package lk.ijse.chat_app.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 public class ClientController {
+    @FXML
+    private VBox emojiBox;
+
+    @FXML
+    private VBox imageBox;
+
+    @FXML
+    private AnchorPane imgPane;
+
+    @FXML
+    private Label labelTime;
+
+    @FXML
+    private Label labelTime1;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private AnchorPane logPane;
+
+    @FXML
+    private AnchorPane msgPane;
+
+    @FXML
+    private ScrollPane pane;
+
+    @FXML
+    private TextField sendText;
+
+    @FXML
+    private TextField userTxt;
+
+    @FXML
+    void emojiSend(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imageOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void loginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void txtValiName(KeyEvent event) {
+
+    }
 }
