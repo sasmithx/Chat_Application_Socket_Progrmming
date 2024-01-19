64eddc384e8f75e7f67557869d4c8ba148826648
sasmithx <sasmithgit@gmail.com>
2024-01-19 09:51:17 +0530

    Server implement

--- src/main/java/lk/ijse/chat_app/ServerInitializer.java
+++ src/main/java/lk/ijse/chat_app/ServerInitializer.java
@@ -1,4 +1,36 @@
 package lk.ijse.chat_app;
 
+import lk.ijse.chat_app.controller.ServerController;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ServerInitializer {
+    private static ArrayList<ServerController> clients = new ArrayList<>();
+
+    private static Socket socket;
+
+    public static void main(String[] args) {
+        System.out.println("Hello" + clients);
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(5003);
+            while(true){
+                System.out.println("Server Listening");
+                socket = serverSocket.accept();
+                System.out.println("connect");
+
+                ServerController serverController = new ServerController(socket,clients);
+                Thread clientThread = new Thread(serverController);
+                clients.add(serverController);
+                System.out.println(clients);
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/lk/ijse/chat_app/controller/ServerController.java
+++ src/main/java/lk/ijse/chat_app/controller/ServerController.java
@@ -1,4 +1,55 @@
 package lk.ijse.chat_app.controller;
 
-public class ServerController {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerController implements Runnable{
+    String msg = " ";
+    private String name;
+    private  DataInputStream dataInputStream;
+    private  DataOutputStream dataOutputStream;
+    private Socket socket;
+    private ArrayList<ServerController> servers;
+
+    public ServerController(Socket socket, ArrayList<ServerController> clients) throws IOException {
+        this.socket = socket;
+        this.servers = clients;
+        this.dataInputStream = new DataInputStream(socket.getInputStream());
+        this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        System.out.println(name);
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            while (!msg.equals("stop")) {
+                name =dataInputStream.readUTF();
+                msg = dataInputStream.readUTF();
+                System.out.println(msg);
+                for (ServerController client : servers) {
+                    System.out.println(client.socket.getPort());
+                    System.out.println(socket.getPort());
+                    System.out.println(client.name);
+                    System.out.println(name + ": " + msg);
+                    if(client.socket.getPort() != socket.getPort()) {
+
+                        client.dataOutputStream.writeUTF(name + ":" + msg);
+                        dataOutputStream.flush();
+                    }
+                }
+//                dataOutputStream.writeUTF("Sasmith");
+//                dataOutputStream.flush();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+
+        }
+    }
 }
